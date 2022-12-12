b98a0cb075c4a68852c9fe5de7afff5078f65dbf
Frankline Che <frankhill.che@gmail.com>
2022-12-12 00:21:52 -0700

    commiting code for infra project

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "AMI_ID" {
+  type = string
+  description = "AMI Id"
+  default = "ami-0ee679ef733e3b8e7"
+}
+
+variable "INSTANCE_TYPE" {
+    type = string
+    description = "Type of ec2 instance"
+    default = "t2.micro"
+  
+}

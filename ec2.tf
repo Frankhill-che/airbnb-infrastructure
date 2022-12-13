e467f36e888078afead8bbd0b3a299ed69c11cf3
Frankline Che <frankhill.che@gmail.com>
2022-12-13 05:43:43 -0700

    checkov scan fix

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "web" {
   instance_type = var.INSTANCE_TYPE
   ebs_optimized = true
   monitoring = true
+  root_block_device {
+  encrypted     = true
+  }
   tags = {
     Name = "airbnb-web-server1"
   }
